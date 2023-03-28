fd1f43cf093ae97ef3871dbe57b4de3388fd4762
DenisLopes <denislopes80@gmail.com>
2023-03-28 12:08:16 -0300

    implementando banco de dados H2

--- src/main/java/com/denis/bookstore/BookstoreApplication.java
+++ src/main/java/com/denis/bookstore/BookstoreApplication.java
@@ -1,13 +1,37 @@
 package com.denis.bookstore;
 
+import com.denis.bookstore.domain.Categoria;
+import com.denis.bookstore.domain.Livro;
+import com.denis.bookstore.repositories.CategoriaRepository;
+import com.denis.bookstore.repositories.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookstoreApplication.class, args);
-	}
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
+    @Autowired
+    private LivroRepository livroRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria categoriaTi = new Categoria(null, "Informatica", "Livro de TI");
+        Livro livroTi = new Livro(null, "Clean Code", "Robert Martin", "Lorem Inpsum", categoriaTi);
+
+        categoriaTi.getLivros().addAll(Arrays.asList(livroTi));
+
+        this.categoriaRepository.saveAll(Arrays.asList(categoriaTi));
+        this.livroRepository.saveAll(Arrays.asList(livroTi));
+    }
 }
--- src/main/java/com/denis/bookstore/domain/Categoria.java
+++ src/main/java/com/denis/bookstore/domain/Categoria.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Entity
-public class Categoria implements Serializable{
+public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
